ee5fe6948fc7e479e3b5e3f4e0661aac6ae5dfa6
Chetan Sarva <chetan@pixelcop.net>
2013-09-19 16:40:31 -0400

    detect test errors and unlock coverage if necessary

--- lib/micron/runner/proc_clazz.rb
+++ lib/micron/runner/proc_clazz.rb
@@ -1,5 +1,5 @@
 
-require "parallel"
+require "lockfile"
 
 module Micron
   class Runner
@@ -12,22 +12,37 @@ module Micron
         results = [] # result methods
         tests = []
 
+        # spawn tests in separate processes
         methods.each do |method|
-          # fork/exec once per method, synchronously
-          ENV["MICRON_TEST_CLASS"] = method.clazz.name
-          ENV["MICRON_TEST_METHOD"] = method.name.to_s
-
-          pid = fork do
-            exec("bundle exec micron --runmethod")
-          end
-
-          tests << { :pid => pid, :method => method }
+          tests << spawn_test(method)
         end
 
-        Process.waitall # wait for all test methods to return
+
+        # wait for all test methods to return
+        finished = []
+        while !tests.empty?
+          tests.each do |test|
+            pid, status = Process.waitpid2(test[:pid], Process::WNOHANG)
+            if !status.nil?
+              puts "process #{pid} exited with status #{status.to_i}"
+              finished << tests.delete(test)
+
+              if status.to_i != 0 then
+                puts "process returned error, forcing unlock"
+                force_unlock(pid)
+              end
+            end
+
+            sleep 0.01
+          end
+        end
+
+        # make sure all locks are cleared, in case there were any errors/coredumps
+        # force_sweep()
+        # force_unlock()
 
         # collect results
-        tests.each do |test|
+        finished.each do |test|
 
           data_file = File.join(ENV["MICRON_PATH"], "#{test[:pid]}.data")
 
@@ -44,6 +59,61 @@ module Micron
         end
 
         @methods = results
+      end
+
+
+      private
+
+      def spawn_test(method)
+        # fork/exec once per method, synchronously
+        ENV["MICRON_TEST_CLASS"] = method.clazz.name
+        ENV["MICRON_TEST_METHOD"] = method.name.to_s
+
+        out, err = IO.pipe, IO.pipe
+        pid = fork do
+          # throw away stdout/err
+          STDOUT.reopen out.last
+          out.last.close
+          STDERR.reopen err.last
+          err.last.close
+          STDOUT.sync = STDERR.sync = true
+          exec("bundle exec micron --runmethod")
+        end
+
+        { :pid => pid, :method => method }
+      end
+
+      def force_sweep
+        puts "sweeping.."
+        %w{coverage .coverage}.each { |cov_dir|
+          file = File.join(Dir.pwd, cov_dir, ".lockfile")
+          next if !File.exists? file
+
+          begin
+            Lockfile.new(file).sweep
+          rescue
+          end
+        }
+      end
+
+      def force_unlock(pid)
+        puts "force unlocking #{pid}"
+
+        %w{coverage .coverage}.each { |cov_dir|
+          file = File.join(Dir.pwd, cov_dir, ".lockfile")
+          next if !File.exists? file
+
+          pidline = File.readlines(file).find { |l| l =~ /^pid:/ }
+          if pid == pidline.split(/:/).last.strip.to_i then
+            puts "found the pid in the lockfile.. deleting!"
+            File.delete(file)
+          end
+
+          # begin
+          #   Lockfile.new(file).unlock
+          # rescue
+          # end
+        }
       end
 
     end
